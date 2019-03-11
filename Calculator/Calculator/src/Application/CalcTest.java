3d127ecb4b5dfb4713769eddc3d1830312896c24
Marcus Wilen <marcus.wilen@hotmail.com>
2019-03-11 21:16:22 +0100

    updating testcase and calculator

--- Calculator/Calculator/src/Application/CalcTest.java
+++ Calculator/Calculator/src/Application/CalcTest.java
@@ -15,6 +15,14 @@ public class CalcTest {
 		
 		assertEquals("4.0", actual);
 	}
+	
+	@Test
+	public void testMultipleAdditionSigns() {
+		String input = "2+2+5+2+3+1+9";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("24.0", actual);
+	}
 
 	@Test
 	public void testSubtraction() {
@@ -25,11 +33,27 @@ public class CalcTest {
 	}
 	
 	@Test
+	public void testMultipleSubtractionSigns() {
+		String input = "3-1-3-7-9-2-3-7-5";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("-34.0", actual);
+	}
+	
+	@Test
 	public void testMultiplication() {
 		String input = "3*2";
 		String actual = calc.calculateExpression(input);
 		
 		assertEquals("6.0", actual);
+	}
+	
+	@Test
+	public void testMutipleMultiplicationSigns() {
+		String input = "3*2*3*7*9";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("1134.0", actual);
 	}
 	
 	@Test
@@ -88,6 +112,62 @@ public class CalcTest {
 		assertEquals("-32.0", actual);
 	}
 	
+	@Test
+	public void testMultipleOperators2() {
+		String input = "4+3-2*5+4";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("1.0", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators3() {
+		String input = "5+6/2*2";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("11.0", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators4() {
+		String input = "7*6/2";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("21.0", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators5() {
+		String input = "5*4+7-3*0+4-10";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("21.0", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators6() {
+		String input = "5*4-5-3*6+2-5*2";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("-11.0", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators7() {
+		String input = "4-5+3*5+2/5-1";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("13.4", actual);
+	}
+	
+	@Test
+	public void testMultipleOperators8() {
+		String input = "11%3+3-4*5%2";
+		String actual = calc.calculateExpression(input);
+		
+		assertEquals("5.0", actual);
+	}
+
 	@Test
 	public void testModulus() {
 		String input = "2%11";
--- Calculator/Calculator/src/Application/Calculator.java
+++ Calculator/Calculator/src/Application/Calculator.java
@@ -1,80 +1,225 @@
 package Application;
+
+import java.sql.Connection;
+
 public class Calculator {
+
+//	Calculator main mehotde
 	public String calculateExpression(String expression){
 		double dres = 0.0;
-		String[] split = expression.split("(?=[*/+-])|(?<=[*/+-])");
-		for (int i = 1; i < split.length; i+= 2) {
-			String op = split[i];
-			double num = Double.parseDouble(split[i+1]);
-				switch (op) {
-				case "*":
-					if (dres != 0.0 && split.length-2 != i) {
-						num = Double.parseDouble(split[i-1]);
-						dres = Multiplication(dres, num);
-					} else if (dres != 0.0 && split.length-2 == i) {
-						dres = Multiplication(dres, num);
-					} else if (dres == 0.0) {
-						dres = Double.parseDouble(split[i-1]);
-						dres = Multiplication(dres, num);
-					}	
-				break;
-				case "/":
-					if (dres != 0.0 && split.length-2 != i) {
-						num = Double.parseDouble(split[i-1]);
-						dres = Division(dres, num);
-					}else if (dres != 0.0 && split.length-2 == i) {
-						dres = Division(dres, num);
-					}else if (dres == 0.0) {
-						dres = Double.parseDouble(split[i-1]);
-						dres = Division(dres, num);
+		double num1 = 0.0;
+		double tempres = 0.0;
+		String[] split = expression.split("(?=[%*/+-])|(?<=[%*/+-])");
+		if (expression.contains("*") || expression.contains("/") || expression.contains("%")) {
+			System.out.println(split.length);
+			for (int i = 1; i < split.length; i+= 2) {
+				String op = split[i];
+				double num = Double.parseDouble(split[i+1]);
+				
+					switch (op) {
+					case "*":
+						if (i > 2 && split.length != i+2) {
+							if (split[i+2].contains("%")) {
+								if (split[i-2].contains("-")) {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, -num);
+								} else {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, num);
+								}
+							} else if (split[i-2].contains("-")) {
+								if (dres != 0.0 && split.length-2 != i) {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, -num);
+									dres += tempres;
+								} else if (dres != 0.0 && split.length-2 == i) {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, -num);
+									dres += tempres;
+								} else if (dres == 0.0) {
+									dres = Double.parseDouble(split[i-1]);
+									dres = Multiplication(-dres, num);
+								}	
+							} else {
+								if (dres != 0.0 && split.length-2 != i) {
+									dres = Multiplication(dres, num);
+								} else if (dres != 0.0 && split.length-2 == i) {
+									dres = Multiplication(dres, num);
+								} else if (dres == 0.0) {
+									dres = Double.parseDouble(split[i-1]);
+									dres = Multiplication(dres, num);
+								}	
+							}
+						} else if (split.length > 3 && split.length != i-2 && i != 1) {
+							if (split[i-2].contains("-")) {
+								if (dres != 0.0 && split.length-2 != i) {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, -num);
+									dres += tempres;
+								} else if (dres != 0.0 && split.length-2 == i) {
+									num1 = Double.parseDouble(split[i-1]);
+									tempres = Multiplication(num1, -num);
+									dres += tempres;
+								} else if (dres == 0.0) {
+									dres = Double.parseDouble(split[i-1]);
+									dres = Multiplication(-dres, num);
+								}	
+							} else {
+								if (dres != 0.0 && split.length-2 != i) {
+									dres = Multiplication(dres, num);
+								} else if (dres != 0.0 && split.length-2 == i) {
+									dres = Multiplication(dres, num);
+								} else if (dres == 0.0) {
+									dres = Double.parseDouble(split[i-1]);
+									dres = Multiplication(dres, num);
+								}	
+							}
+						} else {
+							if (dres != 0.0 && split.length-2 != i) {
+								dres = Multiplication(dres, num);
+							} else if (dres != 0.0 && split.length-2 == i) {
+								dres = Multiplication(dres, num);
+							} else if (dres == 0.0) {
+								dres = Double.parseDouble(split[i-1]);
+								dres = Multiplication(dres, num);
+							}	
+						}
+						break;
+					case "/":
+						if (split.length > 3) {
+							if (split[i-2].contains("*")) {
+								dres = Division(dres, num);
+							}	else if (dres != 0.0 && split.length-2 != i) {
+								num1 = Double.parseDouble(split[i-1]);
+								tempres = Division(num1, num);
+								dres += tempres;
+							}else if (dres != 0.0 && split.length-2 == i) {
+								num1 = Double.parseDouble(split[i-1]);
+								tempres = Division(num1, num);
+								dres += tempres;
+							}else if (dres == 0.0) {
+								dres = Double.parseDouble(split[i-1]);
+								dres = Division(dres, num);
+							}
+						}
+						else if (dres != 0.0 && split.length-2 != i) {
+							num1 = Double.parseDouble(split[i-1]);
+							tempres = Division(num1, num);
+							dres += tempres;
+						}else if (dres != 0.0 && split.length-2 == i) {
+							num1 = Double.parseDouble(split[i-1]);
+							tempres = Division(num1, num);
+							dres += tempres;
+						}else if (dres == 0.0) {
+							dres = Double.parseDouble(split[i-1]);
+							dres = Division(dres, num);
+						}
+						break;
+					case "%":
+						if (i > 2) {							
+							if (split[i-2].contains("*")) {
+								tempres = Modulus(tempres, num);
+								dres += tempres;
+							}
+						}else if (dres != 0.0 && split.length-2 != i) {
+							num = Double.parseDouble(split[i-1]);
+							dres = Modulus(dres, num);
+						}else if (dres != 0.0 && split.length-2 == i) {
+							dres = Modulus(dres, num);
+						}else if (dres == 0.0) {
+							dres = Double.parseDouble(split[i-1]);
+							dres = Modulus(dres, num);
+						}
+						break;
+					default:
+						break;
 					}
-					break;
-				case "%":
-					if (dres != 0.0 && split.length-2 != i) {
-						num = Double.parseDouble(split[i-1]);
-						dres = Modulus(dres, num);
-					}else if (dres != 0.0 && split.length-2 == i) {
-						dres = Modulus(dres, num);
-					}else if (dres == 0.0) {
-						dres = Double.parseDouble(split[i-1]);
-						dres = Modulus(dres, num);
-					}
-					break;
-				default:
-					break;
-				}
+			}
 		}
 		for (int i = 1; i < split.length; i+= 2) {
-			
 			String op = split[i];
 			double num = Double.parseDouble(split[i+1]);
 				switch (op) {
 				case "+":
-					if (dres != 0.0 && split.length-2 != i) {
-						num = Double.parseDouble(split[i-1]);
-						dres = Addition(dres, num);
-					} else if (dres != 0.0 && split.length-2 == i) {
+					if (split.length > 5 && split.length != i+2 && i != 1) {
+						if (split[i+2].contains("*") && split[i-2].contains("*") || split[i+2].contains("/") && split[i-2].contains("/") || split[i+2].contains("/") && split[i-2].contains("*") || split[i+2].contains("*") && split[i-2].contains("/")) {
+							break;
+							
+						} else if(split[i+2].contains("*")){
+							break;
+						} else if (dres != 0.0 && i != 1) {
+							dres = Addition(dres, num);
+						} else if (dres == 0.0) {
+							dres = Double.parseDouble(split[i-1]);
+							dres = Addition(dres, num);
+						} else if (dres != 0.0 && i == 1) {
+							num = Double.parseDouble(split[i-1]);
+							dres = Addition(dres, num);
+						}
+						
+					}
+					else if (dres != 0.0 && i != 1) {
 						dres = Addition(dres, num);
 					} else if (dres == 0.0) {
 						dres = Double.parseDouble(split[i-1]);
+						dres = Addition(dres, num);
+					} else if (dres != 0.0 && i == 1) {
+						num = Double.parseDouble(split[i-1]);
 						dres = Addition(dres, num);
 					}
 				break;
 				case "-":
-					if (dres != 0.0 && split.length-2 != i) {
-						num = Double.parseDouble(split[i-1]);
-						dres = Subtraction(dres, num);
-					} else if (dres != 0.0 && split.length-2 == i) {
-						dres = Subtraction(dres, num);
-					} else if (dres == 0.0) {
-						dres = Double.parseDouble(split[i-1]);
-						dres = Subtraction(dres, num);
+					if (split.length > 3 && i > 4 && expression.contains("*") && split.length != i+2) {
+						if (split[i+2].contains("*") && split[i-4].contains("*")) {
+							break;
+						} else if (split[i+2].contains("*")) {
+							break;
+						}
 					}
-				break;
+					
+					if (split.length > 3 && split.length != i+2) {
+
+						if (split[i+2].contains("*")) {
+							if (dres != 0.0 && i != 1) {
+								num = Double.parseDouble(split[i-1]);
+								dres = Addition(dres, num);
+							} else if (dres == 0.0) {
+								dres = Double.parseDouble(split[i-1]);
+								dres = Addition(dres, num);
+							} else if (dres != 0.0 && i == 1) {
+								num = Double.parseDouble(split[i-1]);
+								dres = Addition(dres, num);
+							}
+						} else {
+							if (dres != 0.0 && i == 1) {
+								num1 = Double.parseDouble(split[i-1]);
+								tempres = Subtraction(num1, num);
+								dres += tempres;
+							} else if (dres != 0.0 && split.length-2 != i) {
+								dres = Subtraction(dres, num);
+							} else if (dres != 0.0 && split.length-2 == i) {
+								dres = Subtraction(dres, num);
+							} else if (dres == 0.0) {
+								dres = Double.parseDouble(split[i-1]);
+								dres = Subtraction(dres, num);
+							}	
+						}
+					} else {
+						if (dres != 0.0 && i != 1) {			
+							dres = Subtraction(dres, num);
+						} else if (dres == 0.0) {
+							dres = Double.parseDouble(split[i-1]);
+							dres = Subtraction(dres, num);
+						} else if (dres != 0.0 && i == 1) {
+							num = Double.parseDouble(split[i-1]);
+							dres = Subtraction(dres, num);
+						}
+					}	
+					break;
 				default:
 					break;
 				}
-		}
+			}
 		String res = Double.toString(dres);
 		return res;
 	}
